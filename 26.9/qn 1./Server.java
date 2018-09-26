2bd276d3c2bb72de47b9b5afc0dd3989f1085eae
Bhargav Krish <bhargav3514@gmail.com>
2018-09-26 16:32:04 +0530

    Add files via upload

---
+++ 26.9/qn 1./Server.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+ 
+
+ 
+public class Server
+{
+    public static void main(String[] args) throws IOException
+    {
+        
+        DatagramSocket ds = new DatagramSocket(5000);
+        byte[] receive = new byte[65535];
+ 
+        DatagramPacket DpReceive = null;
+        while (true)
+        {
+ 
+            // Step 2 : create a DatgramPacket to receive the data.
+            DpReceive = new DatagramPacket(receive, receive.length);
+ 
+            // Step 3 : revieve the data in byte buffer.
+            ds.receive(DpReceive);
+ 
+            System.out.println(data(receive));
+ 
+            // Exit the server if the client sends "bye"
+            if (data(receive).toString().equals("exit"))
+            {
+                System.out.println("Session expired!");
+                break;
+            }
+ 
+            // Clear the buffer after every message.
+            receive = new byte[65535];
+        }
+    }
+ 
+    // A utility method to convert the byte array
+    // data into a string representation.
+    public static StringBuilder data(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
+    }
+}
